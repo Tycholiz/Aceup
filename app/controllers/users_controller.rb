4d976e81c145c7d048de2b818d410abe24899a2c
ngust <nicholasgust@gmail.com>
2017-12-13 15:05:05 -0800

    added session code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id # auto log in
       redirect_to jobs_path
     else
       render :new
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 	
+  get 'sessions/new'
+
+  get 'sessions/create'
+
   get 'users/new'
 
   get 'users/create'
@@ -7,6 +11,8 @@ Rails.application.routes.draw do
   resources :jobs
 
   resources :users, only: [:new, :create]
+
+  resources :sessions, only: [:new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
