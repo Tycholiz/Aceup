9f25e65de4ec9bb26eba55f28562414f5a6f12e0
ngust <nicholasgust@gmail.com>
2017-12-28 10:05:22 -0800

    added phone no regex validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
 	has_one :employer
 
-	validates :email,
+	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create },
 	presence: true
 
 	validates :firstName,
@@ -14,7 +14,7 @@ class User < ApplicationRecord
 	validates :lastName,
 	presence: true
 
-	validates :phoneNo,
+	validates :phoneNo, format: { with: /\d{3}-\d{3}-\d{4}/, message: "Phone number invalid, must be xxx-xxx-xxxx"},
 	presence: true
 
 	validates :role,
