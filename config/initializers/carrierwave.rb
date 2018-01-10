584a460d4644f7da4a7b087f1ae25c271f9d671b
ngust <nicholasgust@gmail.com>
2018-01-10 13:57:00 -0800

    tweaked carrierwave settings for fog/aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,25 +1,25 @@
 # config/initializers/carrierwave.rb
 # This file is not created by default so you might have to create it yourself.
-
   # Use local storage if in development or test
   if Rails.env.development? || Rails.env.test?
     CarrierWave.configure do |config|
       config.storage = :file
     end
   end
-  
-  # Use AWS storage if in production
-  if Rails.env.production?
-    CarrierWave.configure do |config|
-      config.storage = :fog
-    end
-      config.fog_provider = 'fog/aws'
-      config.fog_credentials = {
-          provider:              'AWS',
-          aws_access_key_id:     ENV['S3_KEY'],
-          aws_secret_access_key: ENV['S3_SECRET'],
-          region:                ENV['S3_REGION'],
-      }
-      config.fog_directory  = ENV['S3_BUCKET']
-    end
 
+if Rails.env.production?
+    CarrierWave.configure do |config|
+    #config.storage = :fog
+
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['S3_KEY'],
+      :aws_secret_access_key  => ENV['S3_SECRET'],
+      region: ENV['S3_REGION']
+    }
+    config.fog_directory  = ENV['S3_BUCKET']
+    config.fog_public = false
+    config.storage = :fog
+  end
+end
