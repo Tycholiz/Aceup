7c91043da8172540e4ee66146433cba44f2c1f2f
ngust <nicholasgust@gmail.com>
2018-01-29 16:25:46 -0800

    added education to job model and form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180127023557) do
+ActiveRecord::Schema.define(version: 20180130002029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(version: 20180127023557) do
     t.boolean "commDirect"
     t.boolean "commResidual"
     t.boolean "commLead"
+    t.string "educationLevel"
     t.index ["employer_id"], name: "index_jobs_on_employer_id"
   end
 
