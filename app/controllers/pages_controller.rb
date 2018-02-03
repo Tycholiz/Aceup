356dd207b5e84b2aa178e46af58254de096d8493
ngust <nicholasgust@gmail.com>
2018-02-03 12:38:33 -0800

    made dymanic landing page based on login, fixed spacing issue on seeker header

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,18 @@ class PagesController < ApplicationController
     def show
     	render template: "pages/#{params[:page]}"
     end
+
+    def landing
+    	if current_user.role == "Admin" 
+    		redirect_to admin_users_path, notice: "Welcome back Admin!"
+    	elsif current_user.role == "Seeker" 
+    		@seeker = Seeker.where(user_id: current_user.id).first
+        	redirect_to seeker_path(@seeker), notice: "Welcome back!"
+    	elsif current_user.role == "Employer" 
+    		@employer = Employer.where(user_id: current_user.id).first
+        	redirect_to employer_path(@employer), notice: "Welcome back!"
+        else current_user
+    		render "landing" 
+    	end 
+    end
   end  
