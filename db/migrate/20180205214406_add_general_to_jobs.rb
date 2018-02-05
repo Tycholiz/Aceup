caa74a6c6ccc2a59f8d7d94df16337c61f21b509
ngust <nicholasgust@gmail.com>
2018-02-05 14:05:24 -0800

    got jquery working for general thing

---
+++ db/migrate/20180205214406_add_general_to_jobs.rb
@@ -0,0 +1,5 @@
+class AddGeneralToJobs < ActiveRecord::Migration[5.1]
+  def change
+    add_column :jobs, :general, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180127023557) do
+ActiveRecord::Schema.define(version: 20180205214406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,6 +110,7 @@ ActiveRecord::Schema.define(version: 20180127023557) do
     t.boolean "commDirect"
     t.boolean "commResidual"
     t.boolean "commLead"
+    t.boolean "general"
     t.index ["employer_id"], name: "index_jobs_on_employer_id"
   end
 
