1e0d92c26d43fb55b9e7863cf364adbfa2a9d1b9
ngust <nicholasgust@gmail.com>
2018-02-12 16:17:02 -0800

    added tooltips and tweaked admin dash

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class Admin::DashboardController < Admin::BaseAdminController
 	def landing
     	@users = User.all
-    	@applications = Application.order(:updated_at).reverse_order.page(params[:page]).per(5) 
+    	@applications = Application.order(:updated_at).page(params[:page]).per(5) 
   	end
 end
